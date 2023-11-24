1a6ee127d315be018a7bbb109e89de9ec08305fc
Vladimir Marko <vmarko@google.com>
2023-11-24 12:02:13 +0100

    Simplify boxing followed by unboxing.
    Also mark boxing `valueOf()` intrinsics as never null
    to avoid creating unnecessary `HNullCheck` instructions.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Change-Id: I86e7721e3af6c59407aa2ddfc1bd11bd2fdac83c

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -36,6 +36,7 @@
 #include "class_root-inl.h"
 #include "code_generator.h"
 #include "common_dominator.h"
+#include "intrinsic_objects.h"
 #include "intrinsics.h"
 #include "mirror/class-inl.h"
 #include "scoped_thread_state_change-inl.h"
@@ -3417,6 +3418,21 @@ std::ostream& operator<<(std::ostream& os, HInvokeStaticOrDirect::ClinitCheckReq
     default:
       LOG(FATAL) << "Unknown ClinitCheckRequirement: " << static_cast<int>(rhs);
       UNREACHABLE();
+  }
+}
+
+bool HInvokeStaticOrDirect::CanBeNull() const {
+  if (GetType() != DataType::Type::kReference || IsStringInit()) {
+    return false;
+  }
+  switch (GetIntrinsic()) {
+#define DEFINE_BOXED_CASE(name, unused1, unused2, unused3, unused4) \
+    case Intrinsics::k##name##ValueOf: \
+      return false;
+    BOXED_TYPES(DEFINE_BOXED_CASE)
+#undef DEFINE_BOXED_CASE
+    default:
+      return true;
   }
 }
 
