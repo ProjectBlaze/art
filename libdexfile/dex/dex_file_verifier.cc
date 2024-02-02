77b4aabe93c29bf58591fc750e6d491c77aef7f0
Santiago Aboy Solanes <solanes@google.com>
2024-02-02 15:31:27 +0000

    Add extra checks to CheckIntraStringDataItem regarding OOB
    * in multi byte cases
    * before string end
    
    Based on comments from b/280066537#comment14.
    
    Bug: 283266928
    Bug: 246508656
    Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
    Change-Id: Ifc832663d57e2a0da0a42ed008a51200ea85536b

--- libdexfile/dex/dex_file_verifier.cc
+++ libdexfile/dex/dex_file_verifier.cc
@@ -1649,13 +1649,16 @@ bool DexFileVerifier::CheckIntraStringDataItem() {
   DECODE_UNSIGNED_CHECKED_FROM(ptr_, size);
   const uint8_t* file_end = begin_ + size_;
 
+  size_t available_bytes = static_cast<size_t>(file_end - ptr_);
+  if (available_bytes < size) {
+    ErrorStringPrintf("String data would go beyond end-of-file");
+    return false;
+  }
+  // Eagerly subtract one byte per character.
+  available_bytes -= size;
+
   for (uint32_t i = 0; i < size; i++) {
     CHECK_LT(i, size);  // b/15014252 Prevents hitting the impossible case below
-    if (UNLIKELY(ptr_ >= file_end)) {
-      ErrorStringPrintf("String data would go beyond end-of-file");
-      return false;
-    }
-
     uint8_t byte = *(ptr_++);
 
     // Switch on the high 4 bits.
@@ -1689,10 +1692,11 @@ bool DexFileVerifier::CheckIntraStringDataItem() {
       case 0x0c:
       case 0x0d: {
         // Bit pattern 110x has an additional byte.
-        if (UNLIKELY(ptr_ >= file_end)) {
+        if (available_bytes < 1u) {
           ErrorStringPrintf("String data would go beyond end-of-file");
           return false;
         }
+        available_bytes -= 1u;
 
         uint8_t byte2 = *(ptr_++);
         if (UNLIKELY((byte2 & 0xc0) != 0x80)) {
@@ -1708,10 +1712,11 @@ bool DexFileVerifier::CheckIntraStringDataItem() {
       }
       case 0x0e: {
         // Bit pattern 1110 has 2 additional bytes.
-        if (UNLIKELY((ptr_ >= file_end) || (ptr_ + 1 >= file_end))) {
+        if (available_bytes < 2u) {
           ErrorStringPrintf("String data would go beyond end-of-file");
           return false;
         }
+        available_bytes -= 2u;
 
         uint8_t byte2 = *(ptr_++);
         if (UNLIKELY((byte2 & 0xc0) != 0x80)) {
@@ -1733,16 +1738,18 @@ bool DexFileVerifier::CheckIntraStringDataItem() {
     }
   }
 
-  if (UNLIKELY(ptr_ >= file_end)) {
+  if (available_bytes < 1u) {
     ErrorStringPrintf("String data would go beyond end-of-file");
     return false;
   }
+  available_bytes -= 1u;
 
   if (UNLIKELY(*(ptr_++) != '\0')) {
     ErrorStringPrintf("String longer than indicated size %x", size);
     return false;
   }
 
+  DCHECK_EQ(available_bytes, static_cast<size_t>(file_end - ptr_));
   return true;
 }
 
